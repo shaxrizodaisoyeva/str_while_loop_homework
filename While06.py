3da4133f799352b4da9c299fbf5f8e2e4a863154
shaxrizodaisoyeva <shaxrizodaisoyeva24@gmail.com>
2023-01-09 10:12:48 +0500

    6

--- While06.py
+++ While06.py
@@ -7,3 +7,13 @@ def main(s):
     Returns:
         int: return answer
     """
+    a=0
+    count=0
+    while a<len(s):
+        if s[a]!="a" and s[a]!="e" and s[a]!="i" and s[a]!="o" and s[a]!="u" and s[a]!="A" and s[a]!="E" and s[a]!="I" and s[a]!="O" and s[a]!="U" and  s[a].isalpha()==True:
+            count+=1
+        a+=1
+    return count
+s="abIvfUcGiuYt"
+print(main(s))
+    
