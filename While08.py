2c507645c48eef84312a2421858c1f391da9241b
shaxrizodaisoyeva <shaxrizodaisoyeva24@gmail.com>
2023-01-09 10:18:15 +0500

    8

--- While08.py
+++ While08.py
@@ -6,4 +6,12 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    a=0
+    count=0
+    while a<len(s):
+        if int(s[a])%2!=0 and s[a].isdigit()==True:
+            count+=1
+        a+=1
+    return count
+s="1234567"
+print(main(s))
