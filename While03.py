6f55c4b595ac003c91dcb6c2e177b75fabba1c50
shaxrizodaisoyeva <shaxrizodaisoyeva24@gmail.com>
2023-01-09 09:52:50 +0500

    3

--- While03.py
+++ While03.py
@@ -6,4 +6,13 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    a=0
+    count=0
+    while a<len(s):
+        if s[a].isalpha()==False and s[a].isdigit()==False:
+            count+=1
+        a+=1
+    return count
+s="12%&,.:';gh76"
+print(main(s))
+    
